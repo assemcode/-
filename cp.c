b33759af6da5af826c236a1270eb41b026a38a91
hkwhitecn <1813446392@qq.com>
2017-02-25 22:54:58 +0800

    Add files via upload

---
+++ cp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE 1024
+int main(int argc,char* argv[])
+{
+ char buf[MAX_LINE];  /*缓冲区*/
+ FILE *fp;            /*文件指针*/
+ int len;             /*行字符个数*/
+ if((fp = fopen(argv[1],"r")) == NULL)
+ {
+ perror("fail to read");
+ exit (1) ;
+ }
+ while(fgets(buf,MAX_LINE,fp) != NULL)
+ {
+ len = strlen(buf);
+ buf[len-1] = '\0';  /*去掉换行符*/
+ printf("%s %d \n",buf,len - 1);
+ }
+  return 0;
+}
+
+测试用的
